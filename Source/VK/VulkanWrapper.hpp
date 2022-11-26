ad60cc5bdfa654689a2dcc79348a0a88a997e496
Yang Kyowon <kyowonhorang@gmail.com>
2022-11-26 13:28:44 +0900

    Vulkan Wrapper들을 Native handle로 변환해주는 유틸 함수 추가
    Command Buffer의 Batch 등..

--- Source/VK/VulkanWrapper.hpp
+++ Source/VK/VulkanWrapper.hpp
@@ -7,10 +7,12 @@ namespace sy
 	template <typename VulkanHandleType>
 	class VulkanWrapper : public NamedType
 	{
-		using VulkanDestroyFunctionType = std::function<void(const VulkanInstance& vulkanInstance, VulkanHandleType)>;
+	public:
+		using VulkanDestroyFunction_t = std::function<void(const VulkanInstance& vulkanInstance, VulkanHandleType)>;
+		using Native_t = VulkanHandleType;
 
 	public:
-		VulkanWrapper(const std::string_view name, const VulkanInstance& vulkanInstance, const VulkanDestroyFunctionType destroyFunction) :
+		VulkanWrapper(const std::string_view name, const VulkanInstance& vulkanInstance, const VulkanDestroyFunction_t destroyFunction) :
 			NamedType(name),
 			vulkanInstance(vulkanInstance),
 			destroyFunction(destroyFunction)
@@ -22,14 +24,29 @@ namespace sy
 			destroyFunction(vulkanInstance, handle);
 		}
 
-		[[nodiscard]] VulkanHandleType GetNativeHandle() const { return handle; }
+		[[nodiscard]] Native_t GetNativeHandle() const { return handle; }
 
 	protected:
 		const VulkanInstance& vulkanInstance;
-		const VulkanDestroyFunctionType destroyFunction;
-		VulkanHandleType handle = VK_NULL_HANDLE;
+		const VulkanDestroyFunction_t destroyFunction;
+		Native_t handle = VK_NULL_HANDLE;
 
 	};
+
+	template <typename VulkanWrapperType>
+	std::vector<typename VulkanWrapperType::Native_t> TransformVulkanWrappersToNatives(const CRefSpan<VulkanWrapperType> wrappers)
+	{
+		std::vector<typename VulkanWrapperType::Native_t> natives;
+		natives.resize(wrappers.size());
+		std::transform(wrappers.begin(), wrappers.end(),
+			natives.begin(),
+			[](const CRef<VulkanWrapperType> wrapper) -> VulkanWrapperType::Native_t
+			{
+				return wrapper.get().GetNativeHandle();
+			});
+
+		return natives;
+	}
 }
 
 #define VK_DESTROY_LAMBDA_SIGNATURE(HANDLE_TYPE) [](const VulkanInstance& vulkanInstance, HANDLE_TYPE handle)
