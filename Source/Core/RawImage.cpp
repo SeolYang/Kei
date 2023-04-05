781ee0b38d24dbb9f9cf97c264b9c50551ee7011
Yang Kyowon <seolyang.contact@gmail.com>
2023-04-05 23:36:47 +0900

    Add RawImage

---
+++ Source/Core/RawImage.cpp
@@ -0,0 +1,67 @@
+#include <PCH.h>
+#include <Core/RawImage.h>
+
+namespace sy
+{
+bool RawImage::LoadFromFile(const fs::path& path)
+{
+    const std::string pathStr = path.string();
+    if (!fs::exists(path))
+    {
+        spdlog::error("File {} does not exist.", pathStr);
+        return false;
+    }
+
+    AcquireImageMetadata(pathStr);
+    LoadImageDataFromFile(pathStr);
+
+    return data != nullptr;
+}
+
+void RawImage::AcquireImageMetadata(std::string_view pathStr)
+{
+    if (stbi_is_16_bit(pathStr.data()))
+    {
+        bytesPerChannel = 2;
+    }
+    else if (stbi_is_hdr(pathStr.data()))
+    {
+        bIsHighDynamicRangeImage = true;
+        bytesPerChannel = 4;
+    }
+    else
+    {
+        bytesPerChannel = 1;
+    }
+
+    int width = 0;
+    int height = 0;
+    int numChannels = 0;
+    stbi_info(pathStr.data(), &width, &height, &numChannels);
+    this->extent = Extent3D<uint32_t>{static_cast<uint32_t>(width), static_cast<uint32_t>(height), 1};
+    this->numChannels = static_cast<size_t>(numChannels);
+}
+
+void RawImage::LoadImageDataFromFile(std::string_view pathStr)
+{
+    static constexpr auto StbiDeleter = [](uint8_t* ptr) {
+        stbi_image_free(ptr);
+    };
+
+    switch (bytesPerChannel)
+    {
+        case 1:
+            data = RawImageDataUniquePtr(reinterpret_cast<uint8_t*>(stbi_load(pathStr.data(), nullptr, nullptr, nullptr, 0)),
+                                         StbiDeleter);
+            break;
+        case 2:
+            data = RawImageDataUniquePtr(reinterpret_cast<uint8_t*>(stbi_load_16(pathStr.data(), nullptr, nullptr, nullptr, 0)),
+                                         StbiDeleter);
+            break;
+        case 4:
+            data = RawImageDataUniquePtr(reinterpret_cast<uint8_t*>(stbi_loadf(pathStr.data(), nullptr, nullptr, nullptr, 0)),
+                                         StbiDeleter);
+            break;
+    }
+}
+} // namespace sy
---
+++ Source/Core/RawImage.h
@@ -0,0 +1,38 @@
+#pragma once
+#include <PCH.h>
+
+namespace sy
+{
+class RawImage : public NonCopyable
+{
+private:
+	using RawImageDataUniquePtr = std::unique_ptr<uint8_t, std::function<void(uint8_t*)>>;
+
+public:
+    RawImage() = default;
+    ~RawImage() = default;
+
+    bool LoadFromFile(const fs::path& path);
+
+    [[nodiscard]] bool IsHDR() const { return bIsHighDynamicRangeImage; }
+    [[nodiscard]] uint8_t GetNumChannels() const { return numChannels; }
+    [[nodiscard]] Extent3D<uint32_t> GetExtent() const { return extent; }
+    [[nodiscard]] size_t GetBytesPerChannel() const { return bytesPerChannel; }
+    [[nodiscard]] size_t GetBytesPerPixel() const { return static_cast<size_t>(GetNumChannels()) * GetBytesPerChannel(); }
+    [[nodiscard]] size_t GetSizeBytes() const { return GetBytesPerPixel() * extent.width * extent.height * extent.depth; }
+
+    [[nodiscard]] const uint8_t* GetData() const { return data.get(); }
+    [[nodiscard]] std::span<const uint8_t> GetDataSpan() const { return std::span{reinterpret_cast<const uint8_t*>(data.get()), GetSizeBytes()}; }
+
+private:
+    void AcquireImageMetadata(std::string_view pathStr);
+    void LoadImageDataFromFile(std::string_view pathStr);
+
+private:
+    RawImageDataUniquePtr data;
+    bool bIsHighDynamicRangeImage = false;
+    size_t bytesPerChannel = 0;
+    uint8_t numChannels = 0;
+    Extent3D<uint32_t> extent;
+};
+} // namespace sy
